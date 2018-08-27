fc96c2f14e811a57aa91c54ebffd285af8368b2d
Steven Loria <sloria1@gmail.com>
2018-08-27 18:33:58 -0400

    Upgrade xo

--- src/bundle.js
+++ src/bundle.js
@@ -2,7 +2,7 @@
  * Entry point for UMD builds that bundles
  * sweetalert2's CSS.
  */
-import microfeedback from './';
+import microfeedback from '.';
 import 'sweetalert2/dist/sweetalert2.css';
 
 export default microfeedback;
